8f5b5a93914c9b121caae90c73bc16e40e62ab01
Arold Mauger <aroldmauger2022@gmail.com>
2024-07-30 09:44:01 +0200

    Mise en forme de l'affichage du formulaire avec fonction de validation et messages d'erreur

--- src/components/FormAddEmployee/FormAddEmployee.tsx
+++ src/components/FormAddEmployee/FormAddEmployee.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../../redux/employeeSlice';
 import { states } from "../../data/states";
 import { AppDispatch } from '../../redux/store';
+import Select from 'react-select';
 import "./FormAddEmployee.scss"
 
 interface IFormInput {
@@ -19,87 +20,131 @@ interface IFormInput {
 }
 
 const FormAddEmployee: React.FC = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<IFormInput>();
+  const { register, handleSubmit, formState: { errors }, reset, setValue, getValues } = useForm<IFormInput>();
   const dispatch = useDispatch<AppDispatch>();
 
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+
   const onSubmit: SubmitHandler<IFormInput> = data => {
-    dispatch(addEmployee(data));
-    reset();
+    if (formValidation()) {
+      dispatch(addEmployee(data));
+      reset();
+    }
+  };
+
+  const handleStateChange = (selectedOption: any) => {
+    setValue('state', selectedOption?.value || '');
+    setFormErrors((prevErrors) => ({ ...prevErrors, state: '' }));
+  };
+
+  const handleDepartmentChange = (selectedOption: any) => {
+    setValue('department', selectedOption?.value || '');
+    setFormErrors((prevErrors) => ({ ...prevErrors, department: '' }));
+  };
+
+  const formValidation = (): boolean => {
+    const values = getValues();
+    const newErrors: { [key: string]: string } = {};
+
+    if (!values.firstName) newErrors.firstName = 'First name is required';
+    if (!values.lastName) newErrors.lastName = 'Last name is required';
+    if (!values.dateOfBirth) newErrors.dateOfBirth = 'Date of birth is required';
+    if (!values.startDate) newErrors.startDate = 'Start date is required';
+    if (!values.street) newErrors.street = 'Street name is required';
+    if (!values.city) newErrors.city = 'City name is required';
+    if (!values.state) newErrors.state = 'State is required';
+    if (!values.zipcode) newErrors.zipcode = 'Zip code is required';
+    if (!values.department) newErrors.department = 'Department is required';
+
+    setFormErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <section className='subcontainer'>
-             <div className='input-subcontainer'>
-                <label>First Name</label>
-                <input type="text" {...register("firstName", { required: true, maxLength: 80 })} />
-                {errors.firstName && <p>First name is required</p>}
-            </div>
-
-            <div className='input-subcontainer'>
-                <label>Last Name</label>
-                <input type="text" {...register("lastName", { required: true, maxLength: 100 })} value={"MAUGER"} />
-                {errors.lastName && <p>Last name is required</p>}
-            </div>
-        </section>
-     
-
-        <div className='input-subcontainer one-lign-container'>
-            <label>Date of Birth</label>
-            <input type="date" {...register("dateOfBirth", { required: true })} value={"1995-02-19"}/>
-            {errors.dateOfBirth && <p>Date of birth is required</p>}
+      <section className='subcontainer'>
+        <div className='input-subcontainer'>
+          <label>First Name</label>
+          <input type="text" {...register("firstName", { required: true, maxLength: 80 })} />
+          {errors.firstName && <p className='error-message'>First name is required</p>}
+          {formErrors.firstName && <p className='error-message'>{formErrors.firstName}</p>}
         </div>
 
-        <div className='input-subcontainer one-lign-container'>
-        <label>Start Date</label>
-        <input type="date" {...register("startDate", { required: true })} value={"2024-07-28"}/>
-        {errors.startDate && <p>Start date is required</p>}
+        <div className='input-subcontainer'>
+          <label>Last Name</label>
+          <input type="text" {...register("lastName", { required: true, maxLength: 100 })} value={"MAUGER"} />
+          {errors.lastName && <p className='error-message'>Last name is required</p>}
+          {formErrors.lastName && <p className='error-message'>{formErrors.lastName}</p>}
+        </div>
+      </section>
+
+      <div className='input-subcontainer one-lign-container'>
+        <label>Date of Birth</label>
+        <input type="date" {...register("dateOfBirth", { required: true })} value={"1995-02-19"}/>
+        {errors.dateOfBirth && <p className='error-message'>Date of birth is required</p>}
+        {formErrors.dateOfBirth && <p className='error-message'>{formErrors.dateOfBirth}</p>}
       </div>
 
-      
+      <div className='input-subcontainer one-lign-container'>
+        <label>Start Date</label>
+        <input type="date" {...register("startDate", { required: true })} value={"2024-07-28"}/>
+        {errors.startDate && <p className='error-message'>Start date is required</p>}
+        {formErrors.startDate && <p className='error-message'>{formErrors.startDate}</p>}
+      </div>
+
       <fieldset>
         <legend>ADDRESS</legend>
         <div className='input-subcontainer one-lign-container'>
           <label>Street</label>
           <input type="text" {...register("street", { required: true, maxLength: 250 })} value={"4 ancienne route de Caen"}/>
-          {errors.street && <p>Street name is required</p>}
+          {errors.street && <p className='error-message'>Street name is required</p>}
+          {formErrors.street && <p className='error-message'>{formErrors.street}</p>}
         </div>
         <div className='input-subcontainer one-lign-container'>
           <label>City</label>
           <input type="text" {...register("city", { required: true, maxLength: 250 })} value={"Cabourg"}/>
-          {errors.city && <p>City name is required</p>}
+          {errors.city && <p className='error-message'>City name is required</p>}
+          {formErrors.city && <p className='error-message'>{formErrors.city}</p>}
         </div>
         <div className='input-subcontainer one-lign-container'>
           <label>State</label>
-          <select {...register("state", { required: true })} value={"WY"}>
-            {states.map((state) => (
-              <option key={state.abbreviation} value={state.abbreviation}>
-                {state.name}
-              </option>
-            ))}
-          </select>
-          {errors.state && <p>State is required</p>}
+          <Select 
+            options={states.map((state) => ({ value: state.abbreviation, label: state.name }))}
+            onChange={handleStateChange}
+            className="styled-select"
+            classNamePrefix="react-select"
+          />
+          {errors.state && <p className='error-message'>State is required</p>}
+          {formErrors.state && <p className='error-message'>{formErrors.state}</p>}
         </div>
         <div className='input-subcontainer one-lign-container'>
           <label>Zip Code</label>
           <input type="number" {...register("zipcode", { required: true })} value={"9"}/>
-          {errors.zipcode && <p>Zip code is required</p>}
+          {errors.zipcode && <p className='error-message'>Zip code is required</p>}
+          {formErrors.zipcode && <p className='error-message'>{formErrors.zipcode}</p>}
         </div>
-       
       </fieldset>
+      
       <div className='input-subcontainer one-lign-container'>
-          <label>Department</label>
-          <select {...register("department", { required: true })} value={"Sales"}>
-            <option value="Sales">Sales</option>
-            <option value="Engineering">Engineering</option>
-            <option value="Human Resources">Human Resources</option>
-            <option value="Legal">Legal</option>
-          </select>
-        </div>
+        <label>Department</label>
+        <Select 
+          options={[
+            { value: 'Sales', label: 'Sales' },
+            { value: 'Engineering', label: 'Engineering' },
+            { value: 'Human Resources', label: 'Human Resources' },
+            { value: 'Legal', label: 'Legal' }
+          ]}
+          onChange={handleDepartmentChange}
+          className="styled-select"
+          classNamePrefix="react-select"
+        />
+        {errors.department && <p className='error-message'>Department is required</p>}
+        {formErrors.department && <p className='error-message'>{formErrors.department}</p>}
+      </div>
 
-        <div className='submit-container'>
-            <input type="submit" value="Ajouter"/>
-        </div>
+      <div className='submit-container'>
+        <input type="submit" value="Ajouter"/>
+      </div>
     </form>
   );
 }
